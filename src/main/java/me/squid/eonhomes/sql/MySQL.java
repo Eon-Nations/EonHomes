3d4ffb8006330567e513b6e2c35f2118703c9696
colin-grebenstein <grebensteinforsoftware@gmail.com>
2021-12-10 15:28:19 -0700

    Updated MySQL to work with GCP. Future update needed for SSL connection

--- src/main/java/me/squid/eonhomes/sql/MySQL.java
+++ src/main/java/me/squid/eonhomes/sql/MySQL.java
@@ -9,13 +9,12 @@ import java.sql.SQLException;
 public class MySQL {
 
     EonHomes plugin;
-    private String host, port, database, user, password;
+    private final String host, database, user, password;
     Connection connection;
 
     public MySQL(EonHomes plugin){
         this.plugin = plugin;
         host = plugin.getConfig().getString("SQLHost");
-        port = plugin.getConfig().getString("SQLPort");
         database = plugin.getConfig().getString("SQLDatabase");
         user = plugin.getConfig().getString("SQLUser");
         password = plugin.getConfig().getString("SQLPassword");
@@ -27,7 +26,7 @@ public class MySQL {
 
     public void connectToDatabase() throws SQLException {
         if (!isConnected()) {
-            connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=false", user, password);
+            connection = DriverManager.getConnection("jdbc:mysql://" + host + "/" + database + "?useSSL=false", user, password);
             if (isConnected()) plugin.getLogger().info("Database has succesfully connected");
             else connectToDatabase();
         }
@@ -40,7 +39,9 @@ public class MySQL {
                 connection = null;
             } catch (SQLException exception) {
                 exception.printStackTrace();
-            } catch (NullPointerException e) {}
+            } catch (NullPointerException e) {
+                connection = null;
+            }
         } else {
             connection = null;
         }
